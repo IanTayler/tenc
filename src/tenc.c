c58796432a4e0c1286f3b76693537ef7d19795bf
Ian Tayler <iangtayler@gmail.com>
2017-08-23 01:30:36 -0300

    Add Doxyfile.
    Also added some basic documentation.

---
+++ src/tenc.c
@@ -0,0 +1,32 @@
+/*****************************************************************************
+ * Copyright (c) 2017 by Ian G. Tayler <ian.g.tayler@gmail.com>              *
+ * This file is part of TenC.                                                *
+ *                                                                           *
+ * TenC  is  distributed  as  free  software under the Artistic License 2.0. *
+ * You  should  have received a copy of the license together with the source *
+ * files  for Markovman. It should be in the main directory, in a file named *
+ * 'LICENSE'.                                                                *
+ *****************************************************************************/
+
+ /*!
+ * \mainpage
+ *
+ * \brief Tensor-based deep-learning library written in C and amd64 assembly
+ *
+ * \see https://github.com/IanTayler/tenc.git
+ */
+
+ /**
+  * \file tenc.c
+  * \author Ian G. Tayler
+  *
+  * \brief The main file, where the main cli interface is implemented.
+  *
+  * \see https://github.com/IanTayler/tenc.git
+  */
+#include <float.h>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
