3a283ca173bbc304f5db0baf08dd5a6668ae8ed9
Ian Tayler <iangtayler@gmail.com>
2017-09-01 21:19:07 -0300

    Fix merge errors.

--- test/tensor_tests.c
+++ test/tensor_tests.c
@@ -7,40 +7,14 @@
  * files  for Markovman. It should be in the main directory, in a file named *
  * 'LICENSE'.                                                                *
  *****************************************************************************/
-#include <stdio.h>
 
+#include "tensor.h"
 #include "minunit-ext.h"
 #include "testfunctions.h"
 
-int tests_run = 0;
-char *test_running = "TEST.C";
-
-/*
- *  TESTING THE UNIT TEST LIBRARY.
- */
-char *mu_fine_test()
+/* test initialization and deallocation of shapes */
+char *tensor_new_and_free()
 {
-    test_running = "mu_fine_test";
-    mu_assert("MinUnit isn't working properly", 1);
-    return 0;
-}
+    test_running = "tensor_new_and_free";
 
-char *all_tests()
-{
-    mu_run_test(mu_fine_test);
-
-    return 0;
-}
-
-int main()
-{
-    char *mess = all_tests();
-    if (mess != 0) {
-        printf("Error while running \"%s\":\n%s\n", test_running, mess);
-    } else {
-        printf("OK\n");
-    }
-    printf("Ran %d tests.\n", tests_run);
-
-    return mess != 0;
 }
