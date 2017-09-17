3d9df2449545a43cb4580ebdd4ba1b7516bef477
Ian Tayler <iangtayler@gmail.com>
2017-09-17 16:24:49 -0300

    Implement Neuron

---
+++ src/lib/nn.c
@@ -0,0 +1,27 @@
+/*****************************************************************************
+ * Copyright (c) 2017 by Ian G. Tayler <ian.g.tayler@gmail.com>              *
+ * This file is part of TenC.                                                *
+ *                                                                           *
+ * TenC  is  distributed  as  free  software under the Artistic License 2.0. *
+ * You  should  have received a copy of the license together with the source *
+ * files  for  TenC.  It  should  be  in the main directory, in a file named *
+ * 'LICENSE'.                                                                *
+ *****************************************************************************/
+
+#include "tensor.h"
+#include "tcmath.h"
+#include "nn.h"
+
+float tcnn_activate_neuron(Neuron *n, Tensor *inp)
+{
+    float retv = tc_linear_comb(inp, n->w);
+    retv += n->b;
+    retv = n->actvf(retv);
+    return retv;
+}
+
+void tcnn_free_neuron(Neuron *n)
+{
+    tc_free_tensor(n->w);
+    free(n);
+}
