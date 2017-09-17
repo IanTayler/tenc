b4dad359b26791e458e0d4541e99063f7027b394
Ian Tayler <iangtayler@gmail.com>
2017-09-17 14:07:59 -0300

    Remove tc_long_btfy and tc_long_pprint
    They hadn't been implemented and I don't plan on implementing them any time soon.

--- src/utils/errorutils.c
+++ src/utils/errorutils.c
@@ -19,7 +19,11 @@ void alert_unequal_shapes(Tensor *t1, Tensor *t2, char *fn_name)
     if (!tc_shape_equal(t1->shape, t2->shape)) {
         /* Shapes aren't the same */
         fprintf(stderr, "Unequal shapes in %s.\n", fn_name);
-        fprintf(stderr, "Shape 1: %s\n", tc_btfy_shape(t1->shape));
-        fprintf(stderr, "Shape 2: %s\n", tc_btfy_shape(t2->shape));
+        char *bty_shape_1 = tc_btfy_shape(t1->shape);
+        char *bty_shape_2 = tc_btfy_shape(t2->shape);
+        fprintf(stderr, "Shape 1: %s\n", bty_shape_1);
+        fprintf(stderr, "Shape 2: %s\n", bty_shape_2);
+        free(bty_shape_1);
+        free(bty_shape_2);
     }
 }
