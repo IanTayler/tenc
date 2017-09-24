5be0ea2672ecc6ca28a916b77d2541b50ae25bbc
IanTayler <iangtayler@gmail.com>
2017-09-23 23:00:39 -0300

    Fix errors.

--- src/include/tensor.h
+++ src/include/tensor.h
@@ -26,25 +26,25 @@
 */
 void tc_initialize();
 /**
- * \brief Integer type for dimension sizes in shapes.
- */
+* \brief Integer type for dimension sizes in shapes.
+*/
 typedef int64_t int_shape_t;
 /**
- * \brief Type for Tensor shapes.
- */
+* \brief Type for Tensor shapes.
+*/
 typedef struct {
     int len; /**<Number of dimensions in the shape.*/
     int_shape_t *arr; /**<Array holding the size of each dimension.*/
 } Shape;
 
 /**
- * \brief Dynamically allocates a Shape.
- *
- * \param length Length of the shape.
- * \param shape_arr Array with the dimensions' size.
- *
- * \return A pointer to a Shape. Should later be deallocated with free_shape.
- */
+* \brief Dynamically allocates a Shape.
+*
+* \param length Length of the shape.
+* \param shape_arr Array with the dimensions' size.
+*
+* \return A pointer to a Shape. Should later be deallocated with free_shape.
+*/
 Shape *tc_new_shape(int length, int_shape_t *shape_arr);
 
 /**
@@ -100,19 +100,20 @@ Shape *tc_4d_shape(int_shape_t dim0, int_shape_t dim1,
 Shape *tc_copy_shape(Shape *shape);
 
 /**
- * \brief Deallocates a Shape.
- *
- * \param ptr Pointer to a Shape.
- */
+* \brief Deallocates a Shape.
+*
+* \param ptr Pointer to a Shape.
+*/
 void tc_free_shape(Shape *ptr);
+
 /**
- * \brief Gets the amount of values in a Tensor of a specific Shape.
- *
- * \param ptr Shape.
- *
- * \return int_shape_t holding the number of values in a Tensor that has the
- * input shape.
- */
+* \brief Gets the amount of values in a Tensor of a specific Shape.
+*
+* \param ptr Shape.
+*
+* \return int_shape_t holding the number of values in a Tensor that has the
+* input shape.
+*/
 int_shape_t tc_shape_size(Shape *shape);
 
 /**
@@ -126,30 +127,30 @@ int_shape_t tc_shape_size(Shape *shape);
 bool tc_shape_equal(Shape *s1, Shape *s2);
 
 /**
- * \brief Type for a float tensor.
- *
- * A Tensor is simply represented as an array of floats, with functions capable
- * of accessing the right value by index, etc.
- *
- * Things to note include:
- *     - These Tensors are statically shaped (i.e. shape is known at
- *       compile-time)
- *     - They hold floats exclusively. Be careful with floating point rounding
- *       error! This library won't hold your hand with respect to that.
- */
+* \brief Type for a float tensor.
+*
+* A Tensor is simply represented as an array of floats, with functions capable
+* of accessing the right value by index, etc.
+*
+* Things to note include:
+*     - These Tensors are statically shaped (i.e. shape is known at
+*       compile-time)
+*     - They hold floats exclusively. Be careful with floating point rounding
+*       error! This library won't hold your hand with respect to that.
+*/
 typedef struct {
     Shape *shape; /**<Tensor's size in each dimension.*/
     float *value_array; /**<Array with the Tensor's values*/
 } Tensor;
 
 /**
- * \brief Dynamically allocates a Tensor.
- *
- * \param shape Tensor's shape.
- * \param value_array One-dimensional array with all the values.
- *
- * \return Pointer to a Tensor. Should be deallocated with free_tensor.
- */
+* \brief Dynamically allocates a Tensor.
+*
+* \param shape Tensor's shape.
+* \param value_array One-dimensional array with all the values.
+*
+* \return Pointer to a Tensor. Should be deallocated with free_tensor.
+*/
 Tensor *tc_new_tensor(Shape *shape, float *value_array);
 
 /**
@@ -178,82 +179,84 @@ Tensor *tc_shape_to_tensor(Shape *shape);
 *
 * \brief ptr Pointer to a Tensor.
 */
-void tc_free_tensor_w_shared_shape(Tensor *ptr)
+void tc_free_tensor_w_shared_shape(Tensor *ptr);
 
 /**
- * \brief Deallocates a Tensor.
- *
- * \param ptr Pointer to a Tensor.
- */
+* \brief Deallocates a Tensor.
+*
+* \param ptr Pointer to a Tensor.
+*/
 void tc_free_tensor(Tensor *ptr);
 
 /**
- * \brief Create a Tensor and initialize all elements to a certain value.
- *
- * \param shape Shape of the returned Tensor.
- * \param value Float that will be used to initialize all elements in the
- * Tensor.
- *
- * \return Pointer to a Tensor.
- *
- * \note You should later free the Tensor with free_tensor.
- *
- * \see tc_zeros tc_zeros_like tc_ones tc_ones_like
- */
+* \brief Create a Tensor and initialize all elements to a certain value.
+*
+* \param shape Shape of the returned Tensor.
+* \param value Float that will be used to initialize all elements in the
+* Tensor.
+*
+* \return Pointer to a Tensor.
+*
+* \note You should later free the Tensor with tc_free_tensor.
+*
+* \see tc_zeros tc_zeros_like tc_ones tc_ones_like
+*/
 Tensor *tc_fill(Shape *shape, float value);
 
 /**
- * \brief Create a Tensor and initialize it to all zeros.
- *
- * \param shape Shape of the returned Tensor.
- *
- * \return Pointer to a Tensor.
- *
- * \note You should later free the Tensor with free_tensor.
- *
- * \see tc_fill tc_zeros_like tc_ones tc_ones_like
- */
+* \brief Create a Tensor and initialize it to all zeros.
+*
+* \param shape Shape of the returned Tensor.
+*
+* \return Pointer to a Tensor.
+*
+* \note You should later free the Tensor with tc_free_tensor.
+*
+* \see tc_fill tc_zeros_like tc_ones tc_ones_like
+*/
 Tensor *tc_zeros(Shape *shape);
 
 /**
- * \brief Create a Tensor and initialize it to all zeros, copying the shape
- * from another Tensor.
- *
- * \param t Tensor from which to copy the shape.
- *
- * \return Pointer to a Tensor.
- *
- * \note You should later free the Tensor with free_tensor.
- *
- * \see tc_fill tc_zeros tc_ones tc_ones_like
- */
+* \brief Create a Tensor and initialize it to all zeros, copying the shape
+* from another Tensor.
+*
+* \param t Tensor from which to copy the shape.
+*
+* \return Pointer to a Tensor.
+*
+* \note You should later free the Tensor with tc_free_tensor_w_shared_shape.
+* And the initial Tensor with tc_free_tensor.
+*
+* \see tc_fill tc_zeros tc_ones tc_ones_like
+*/
 Tensor *tc_zeros_like(Tensor *t);
 
 /**
- * \brief Create a Tensor and initialize it to all ones.
- *
- * \param shape Shape of the returned Tensor.
- *
- * \return Pointer to a Tensor.
- *
- * \note You should later free the Tensor with free_tensor.
- *
- * \see tc_fill tc_zeros tc_zeros_like tc_ones_like
- */
+* \brief Create a Tensor and initialize it to all ones.
+*
+* \param shape Shape of the returned Tensor.
+*
+* \return Pointer to a Tensor.
+*
+* \note You should later free the Tensor with free_tensor.
+*
+* \see tc_fill tc_zeros tc_zeros_like tc_ones_like
+*/
 Tensor *tc_ones(Shape *shape);
 
 /**
- * \brief Create a Tensor and initialize it to all ones, copying the shape
- * from another Tensor.
- *
- * \param t Tensor from which to copy the shape.
- *
- * \return Pointer to a Tensor.
- *
- * \note You should later free the Tensor with free_tensor.
- *
- * \see tc_fill tc_zeros tc_zeros_like tc_ones
- */
+* \brief Create a Tensor and initialize it to all ones, copying the shape
+* from another Tensor.
+*
+* \param t Tensor from which to copy the shape.
+*
+* \return Pointer to a Tensor.
+*
+* \note You should later free the Tensor with tc_free_tensor_w_shared_shape.
+* And the initial Tensor with tc_free_tensor.
+*
+* \see tc_fill tc_zeros tc_zeros_like tc_ones
+*/
 Tensor *tc_ones_like(Tensor *t);
 
 /**
@@ -262,7 +265,7 @@ Tensor *tc_ones_like(Tensor *t);
 * \param shape Shape.
 * \param fn Function with no arguments returning a float.
 *
-* \note Generating a Tensor using this fucntion will be substantially slower
+* \note Generating a Tensor using this function will be substantially slower
 * than using tc_fill and similar functions. The main reason being that we don't
 * parallelize this function to avoid problems with repeated results when using
 * a random generator function (probably the most common use case for this
