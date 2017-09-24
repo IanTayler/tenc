f00ba501373dd0c03c746bdae952af057a7e3977
IanTayler <iangtayler@gmail.com>
2017-09-23 23:41:54 -0300

    Add basic tests for Shape initializer and deallocator.

--- test/tensor_tests.c
+++ test/tensor_tests.c
@@ -8,13 +8,40 @@
  * 'LICENSE'.                                                                *
  *****************************************************************************/
 
+#include <stdlib.h>
+
 #include "tensor.h"
 #include "minunit-ext.h"
 #include "testfunctions.h"
 
+char *shape_new_and_free()
+{
+    test_running = "shape_new_and_free";
+    int shapelen = 3;
+    int_shape_t *shapearr = malloc(sizeof(*shapearr) * shapelen);
+    shapearr[0] = 2;
+    shapearr[1] = 4097;
+    shapearr[2] = 45000;
+    Shape *s = tc_new_shape(shapelen, shapearr);
+    Shape *s2 = malloc(sizeof(*s2));
+    s2->len = shapelen;
+    s2->arr = shapearr;
+    mu_assert_running(
+        "tc_new_shape is failing to produce the expected Shape",
+        tc_shape_equal(s, s2),
+        {
+            tc_free_shape(s);
+            /* This will suffice because they share pointers */
+            free(s2);
+        }
+    );
+    tc_free_shape(s);
+    free(s2);
+    return 0;
+}
 /* test initialization and deallocation of shapes */
 char *tensor_new_and_free()
 {
     test_running = "tensor_new_and_free";
-
+    return 0;
 }
